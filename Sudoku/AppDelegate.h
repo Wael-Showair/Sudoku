63e20e6a4275fac0f4936553927c268e9238a564
show0017 <show0017@algonquinlive.com>
2016-02-10 16:05:09 -0500

    Initial Commit

---
+++ Sudoku/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sudoku
+//
+//  Created by Wael Showair on 2016-02-10.
+//  Copyright © 2016 Algonquin College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sudoku/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sudoku
+//
+//  Created by Wael Showair on 2016-02-10.
+//  Copyright © 2016 Algonquin College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
